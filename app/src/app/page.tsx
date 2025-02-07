fbd1237e5650be64f5d977226b700db7b3e56042
Jorge Ramirez <jorge.ramirez1787@gmail.com>
2025-02-07 11:13:40 +0100

    fixed non-escaped entity

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,18 +1,17 @@
-// app/page.tsx
 export default function Home() {
     return (
-        <main style={{ padding: "20px", fontFamily: "Arial, sans-serif" }}>
-          <h1>There's nothing supposed to be here for now.</h1>
-          <p>
-            Go to <a href="/map">/map</a> or <a href="/controlPanel">/controlPanel</a>.
-          </p>
-          <p>
-            For an overview of the contexts, visit{" "}
-            <a href="/examples/contexts/multi-component">
-              /examples/contexts/multi-component
-            </a>.
-          </p>
-        </main>
-      );
+      <main style={{ padding: "20px", fontFamily: "Arial, sans-serif" }}>
+        <h1>There&apos;s nothing supposed to be here for now.</h1>
+        <p>
+          Go to <a href="/map">/map</a> or <a href="/controlPanel">/controlPanel</a>.
+        </p>
+        <p>
+          For an overview of the contexts, visit{" "}
+          <a href="/examples/contexts/multi-component">
+            /examples/contexts/multi-component
+          </a>.
+        </p>
+      </main>
+    );
   }
   
