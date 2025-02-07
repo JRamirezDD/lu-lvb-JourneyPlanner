9c6bf5c05b8b8559ec1f5add6ad59224bcffbef5
Jorge Ramirez <jorge.ramirez1787@gmail.com>
2025-02-07 11:14:18 +0100

    Merge pull request #66 from JRamirezDD/CI-CD
    Ci cd

--- app/src/app/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default function Home() {
-  redirect("/en"); // Perform redirect
-  return <div>Redirecting...</div>; // Ensure it returns a React component
-}
