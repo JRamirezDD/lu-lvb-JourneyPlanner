df6661916f2d19cb34a0131f2ae66637a287198a
Jorge Ramirez <jorge.ramirez1787@gmail.com>
2025-02-07 11:25:59 +0100

    fix attempt

--- app/next.config.ts
+++ app/next.config.ts
@@ -1,15 +1,20 @@
 const createNextIntl = require('next-intl/plugin');
-const withNextIntl = createNextIntl();
+
+const withNextIntl = createNextIntl({
+  defaultLocale: 'en', // Set your default language
+  locales: ['en', 'de'], // Add all supported locales
+  localeDetection: false, // Disables automatic cookie-based locale detection
+});
 
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-    output: "export",
-    images: {
-        unoptimized: true // Needed for GitHub pages deployments
-    },
-    basePath: '/lu-lvb-JourneyPlanner',
-    assetPrefix: '/lu-lvb-JourneyPlanner/',
+  output: "export",
+  images: {
+    unoptimized: true, // Needed for GitHub Pages
+  },
+  basePath: '/lu-lvb-JourneyPlanner',
+  assetPrefix: '/lu-lvb-JourneyPlanner/',
 };
 
 export default withNextIntl(nextConfig);
