b7521917422f488715f9ac4d77317bb6e8223292
Jorge Ramirez <jorge.ramirez1787@gmail.com>
2025-02-20 20:46:29 +0100

    proxy pt1

--- app/src/api/httpClient.ts
+++ app/src/api/httpClient.ts
@@ -2,14 +2,13 @@ import axios, { AxiosInstance } from 'axios';
 
 // Create an Axios instance
 const httpClient: AxiosInstance = axios.create({
-    baseURL: `${process.env.NEXT_PUBLIC_API_BASE_URL}`, // proxy the request for CORS. end-solution, implement server-side proxy
+    baseURL: `${process.env.NEXT_PUBLIC_PROXY_ADDRESS}`, // proxy the request for CORS. end-solution, implement server-side proxy
     timeout: 10000, // Set a timeout
 });
 
 // Add a request interceptor to include the API key
 httpClient.interceptors.request.use(
     (config) => {
-        config.headers['X-API-Key'] = `${process.env.NEXT_PUBLIC_API_KEY}`;
         config.headers['Content-Type'] = 'application/json:';
 
         return config;
--- app/src/api/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './autocompleteService/autocompleteService';
-export * from './autocompleteService/dto/autocompleteitemResponse';
-export * from './autocompleteService/dto/searchparamsRequest';
-import dotenv from 'dotenv';
-dotenv.config();
-
