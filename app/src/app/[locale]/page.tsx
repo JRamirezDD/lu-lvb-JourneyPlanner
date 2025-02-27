dcc4e8e6c48f468f82c9e5b7f6203ccc9e920e6c
MarkzUni <m.gonzalezfernandez@lancaster.ac.uk>
2025-02-27 15:50:26 +0100

    Attempt main main?

--- app/src/app/[locale]/page.tsx
+++ app/src/app/[locale]/page.tsx
@@ -1,18 +1,12 @@
-import MainView from "@/components/MainView";
 import { generateStaticParams } from "./generateStaticParams";
 export { generateStaticParams };
 
-export default async function LocaleHome({ params }: { params: Promise<{ locale: string }> }) {
-  const { locale } = await params;
-  const basePath = process.env.NEXT_PUBLIC_BASE_PATH || "";
+import MainView from "@/components/MainView";
 
+export default function MainPage() {
   return (
-    <div style={{ display: "flex", flexDirection: "column", height: "100%" }}>
-      {/* Main content area */}
-      <main style={{ flex: 1, display: "flex", overflow: "hidden", height: "100%", margin:0, padding:0 }}>
-        {/* MainView fills this section */}
+    <div id="app" style={{ width: "100%", height: "100%" }}>
         <MainView />
-      </main>
-    </div>
+      </div>
   );
 }
